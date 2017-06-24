56313dcabf3fc02752a696526dda064fa5d34102
Nia Charrington <nia@charrington.com>
2017-06-24 09:22:05 -0700

    edits

--- app.py
+++ app.py
@@ -26,9 +26,9 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = processRequest(req)
+    #res = processRequest(req)
 
-    res = json.dumps(res, indent=4)
+    res = json.dumps(req, indent=4)
     # print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
