96746585b3743d29c4dd7abf3108937193b49724
Nia Charrington <nia@charrington.com>
2017-06-24 08:18:28 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def processRequest(req):
+    if req.get("result").get("action") != "DuckDuckGoInstantAnswer":
+        return {}
+    baseurl = "http://api.duckduckgo.com/?q=DuckDuckGo"
+    yql_query = makeYqlQuery(req)
+    if yql_query is None:
+        return {}
+    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    result = urlopen(yql_url).read()
+    data = json.loads(result)
+    res = makeWebhookResult(data)
+    return res
+
+
+def makeYqlQuery(req):
+    result = req.get("result")
+    query = result.get("resolvedQuery")
+    if query is None:
+        return None
+
+    return query
+
+
+def makeWebhookResult(data):
+    query = data.get('query')
+    if query is None:
+        return {}
+
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    answer = result.get('answer')
+    if answer is None:
+        return {}
+
+    # item = channel.get('item')
+    # location = channel.get('location')
+    # units = channel.get('units')
+    # if (location is None) or (item is None) or (units is None):
+    #     return {}
+    #
+    # condition = item.get('condition')
+    # if condition is None:
+    #     return {}
+
+    # print(json.dumps(item, indent=4))
+
+    speech = answer
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-duckduckgo-webhook"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0')
